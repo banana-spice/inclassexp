24f1e90a6e92afb08644e288871f5a51ae6f4fa7
BRADEN <vb0517423@otc.edu>
2017-11-08 09:12:45 -0600

    More

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var express = require('express')
 var app = express()
+var url = require("url");
 var path = require("path");
  
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, "/index.html"));
+  //res.send(url.parse(req.url).pathname);
+})
+app.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname, "/about.html"));
+  //res.send(url.parse(req.url).pathname);
 })
  
 app.listen(3000)
